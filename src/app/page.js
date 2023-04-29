7e730c0d4c87cd92eac1a33d95bfc925040b6b5e
Johnny <johnnyomoseni100@gmail.com>
2023-04-29 05:13:07 +0100

    Fixed issues

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,3 @@
-"use client";
-
 import Header from "../../components/Header/Header";
 import HomeMain from "../../components/Pages/Home/Main/HomeMain";
 import Footer from "../../components/Footer/Footer";
